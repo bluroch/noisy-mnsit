1a94737ea8cd8a8b54c192dd1d948308a5db4125
Botond, Kocsis <kocsis@inf.u-szeged.hu>
2023-12-01 21:11:38 +0100

    Add model saving and loading Also add image saving
    Co-authored-by: Chaosz <kuladam@inf.u-szeged.hu>

--- runner.py
+++ runner.py
@@ -4,15 +4,16 @@ import os
 # set this to 3 so tensorflow doesnt spam terminal if not GPU is present
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
 
+import glob
 import sys
 
 import keras
+import matplotlib.pyplot as plt
 import numpy as np
 import numpy.typing
 import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
-import tensorflow
 from keras.datasets import mnist
 from keras.layers import (Activation, BatchNormalization, Conv2D, Dense,
                           Dropout, Flatten, GaussianNoise, MaxPooling2D)
@@ -30,34 +31,38 @@ img_rows, img_cols = 28, 28
 # Terminal flags for ease of use
 verbose = False
 debug = False
+train = False
 for arg in sys.argv:
     if arg == "verbose":
         verbose = True
     if arg == "debug":
         debug = True
+    if train == "true":
+        train = True
 
 # Models
 
 
 def gaussian_noise():
     # Gaussian Noise has a regularization effect to a certain limit
-    model = Sequential()
+    model = Sequential(name="Gaussian")
     model.add(
         Conv2D(
             16, kernel_size=(3, 3), padding="same", input_shape=(img_rows, img_cols, 1)
         )
     )
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(GaussianNoise(0.1))
-    model.add(Dense(128, activation="relu"))
     model.add(Flatten())
+    model.add(Dense(32, activation="linear"))
+    model.add(GaussianNoise(0.3))
+    model.add(Activation(keras.activations.linear))
     model.add(Dense(num_classes, activation="softmax"))
 
     return model
 
 
 def base_model():
-    model = Sequential()
+    model = Sequential(name="base")
     model.add(
         Conv2D(
             16, kernel_size=(3, 3), padding="same", input_shape=(img_rows, img_cols, 1)
@@ -169,6 +174,8 @@ def generate_noisy_datasets(dataset) -> None:
 def main():
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
+    models: dict = {}
+
     if debug:
         print("elements reduced from: ", x_train.shape)
         x_train = numpy.split(x_train, 10)[0]
@@ -186,49 +193,76 @@ def main():
     funcs = func_list()
 
     scores = []
-    for i, func in enumerate(funcs):
-        model = func()
+    if train:
+        for i, func in enumerate(funcs):
+            model = func()
 
-        model.compile(
-            loss=keras.losses.categorical_crossentropy,
-            optimizer=keras.optimizers.Adam(),
-            metrics=["accuracy"],
-        )
+            model.compile(
+                loss=keras.losses.categorical_crossentropy,
+                optimizer=keras.optimizers.Adam(),
+                metrics=["accuracy"],
+            )
+            model.fit(
+                x_train, y_train, epochs=epochs, validation_data=(x_test, y_test), verbose=0
+            )
 
-        model.fit(
-            x_train, y_train, epochs=epochs, validation_data=(x_test, y_test), verbose=0
-        )
+            if verbose:
+                model.summary()
+
+            models[func().name] = model
+            model.save(f"{func().name}.keras", save_format="keras")
+
+            score = model.evaluate(x_test, y_test, verbose=0)
+
+            scores.append((score, func.__name__))
+            print("\nFunction: " + func.__name__)
+            print("\tTest loss: ", scores[i][0][0])
+            print("\tTest accuracy: ", scores[i][0][1])
 
         if verbose:
-            model.summary()
+            print("\n", scores)
 
-        score = model.evaluate(x_test, y_test, verbose=0)
+        average_acc = 0
+        best_acc = scores[0]
+        for score in scores:
+            (data, name) = score
+            average_acc += data[1]
+            if data[1] > best_acc[0][1]:
+                best_acc = score
 
-        scores.append((score, func.__name__))
-        print("\nFunction: " + func.__name__)
-        print("\tTest loss: ", scores[i][0][0])
-        print("\tTest accuracy: ", scores[i][0][1])
+        average_acc /= len(scores)
+        print("\n")
+        print("Average accuracy:", average_acc)
+        print("Best accuracy:", best_acc[1])
 
-    if verbose:
-        print("\n", scores)
+        print("{0:^20}|{1:^10}".format("name", "deviation (from avg)"))
+        for score in scores:
+            (data, name) = score
+            print("{0:^20}|{1:^10.3f}".format(name, data[1] / average_acc))
 
-    average_acc = 0
-    best_acc = scores[0]
-    for score in scores:
-        (data, name) = score
-        average_acc += data[1]
-        if data[1] > best_acc[0][1]:
-            best_acc = score
+    else:
+        models: dict = {}
+        model_paths = glob.glob("*.keras")
+        for path in model_paths:
+            models[path] = keras.models.load_model(path)
 
-    average_acc /= len(scores)
-    print("\n")
-    print("Average accuracy:", average_acc)
-    print("Best accuracy:", best_acc[1])
 
-    print("{0:^20}|{1:^10}".format("name", "deviation (from avg)"))
-    for score in scores:
-        (data, name) = score
-        print("{0:^20}|{1:^10.3f}".format(name, data[1] / average_acc))
+    datasets: dict = {}
+    paths = glob.glob("*.parquet")
+    for path in paths:
+        datasets[path] = load_dataset(path)
+
+    os.makedirs("images", exist_ok=True)
+
+    for name, dataset in datasets.items():
+        print(f"Dataset: {name}")
+        for model_name, model in models.items():
+            score = model.evaluate(dataset, y_test, verbose=0)
+            print(f"\t{model_name}: {score[1]}")
+        for i in range(0, 2):
+            plt.imshow(dataset[i])
+            plt.savefig(f"images/{name.replace('.parquet', '')}_img_{i}.png")
+            plt.close()
 
     return
 
